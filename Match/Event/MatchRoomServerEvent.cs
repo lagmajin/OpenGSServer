b815a25f82568a18649a06886c52db1da5007548
lagmajin <lagmajin@gmail.com>
2025-02-19 14:15:16 +0900

    Add Timestamp

---
+++ Match/Event/MatchRoomServerEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenGSServer
+{
+    internal abstract class IServerMatchEvent
+    {
+        public MatchRoom MatchRoom { get; }
+
+        public IServerMatchEvent(MatchRoom room)
+
+        {
+            MatchRoom = room;
+
+        }
+    }
+    internal class ServerMatchStartEvent:IServerMatchEvent
+    {
+        public ServerMatchStartEvent(MatchRoom room):base(room)
+        {
+            
+
+
+        }
+
+    }
+    internal class ServerMatchEndEvent : IServerMatchEvent
+    {
+        public readonly MatchResult matchResult;
+
+        public ServerMatchEndEvent(MatchResult result,MatchRoom room) : base(room)
+        {
+            matchResult = result;
+            
+
+        }
+    }
+
+
+
+
+}
